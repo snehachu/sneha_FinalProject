7f27178c05f7e19a540bc40d0233aab6212a83e6
snehachu <sneha.chudasma@gmail.com>
2023-12-18 21:37:15 +1100

    update

--- proj.js
+++ proj.js
@@ -9,3 +9,15 @@ function closeCustomModal() {
     document.getElementById("custom-modal").style.display = "none";
 }
 
+function addToCart(){
+
+    const cakeType = document.getElementById('cakeType').value;
+    const cakeSize = document.getElementById('cakeSize').value;
+    const quantity = document.getElementById('quantity').value;
+}
+
+
+const cartItem = document.createElement('li');
+cartItem.textContent =`${quantity} ${cakeSize} ${cakeType} cake(s)`;
+
+document.getElementById('cartItems').appendChild(cartItem);
